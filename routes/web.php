6875d4a5f327e3d88feb32deb912e89f5d412e48
SniksaX <papoosep1@gmail.com>
2026-04-15 16:39:29 +0200

    [ADD] activité 8

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/dashboard', function () { return view('dashboard');})
-    ->middleware(['auth', 'verified', 'role:admin'])
-    ->name('dashboard');
+    Route::resource('products', ProductController::class);
+
+    Route::get('/dashboard', function () { 
+        return view('dashboard'); 
+    })->middleware(['verified', 'role:admin'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
